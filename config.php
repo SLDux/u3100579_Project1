471a2afbbe1ba279a4a0cd5d805a009e79e88c26
SLDux <47551368+SLDux@users.noreply.github.com>
2019-03-26 16:06:16 +1100

    update for online hosting

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "project_1";
+$host = "sql306.epizy.com";
+$username = "epiz_23660656";
+$password = "LVvFgxRLe";
+$dbname = "epiz_23660656_crudtracker";
 $dsn = "mysql:host=$host;dbname=$dbname"; 
 $options = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
